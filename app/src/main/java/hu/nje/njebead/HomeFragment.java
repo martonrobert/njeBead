ba78923835537c4247ce0247ab1ece2966d0b62c
Kovaben20 <129586198+Kovaben20@users.noreply.github.com>
2024-01-16 17:29:00 +0100

    Kezdőlap, nézet módosítása
    HomeFragment és TeamListFragment  között navigáció

--- app/src/main/java/hu/nje/njebead/HomeFragment.java
+++ app/src/main/java/hu/nje/njebead/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,11 +12,16 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.button.MaterialButton;
+
 import java.util.List;
 
 import hu.nje.njebead.models.Team;
 
 public class HomeFragment extends Fragment {
+
+    MaterialButton button;
+    TextView textView;
 
     public static HomeFragment newInstance() {
         HomeFragment fragment = new HomeFragment();
@@ -32,6 +38,41 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.home_fragment, container, false);
 
+        button = rootView.findViewById(R.id.button_Id);
+        textView = rootView.findViewById(R.id.TextView_Id);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TeamListFragment teamListFragment = new TeamListFragment();
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, teamListFragment).commit();
+            }
+        });
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TeamListFragment teamListFragment = new TeamListFragment();
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, teamListFragment).commit();
+            }
+        });
+
         return rootView;
     }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Ellenőrizze, hogy a HomeFragment látható-e
+        if (isVisible()) {
+            // A HomeFragment látható, tehát megjelenítjük a gombot
+            button.setVisibility(View.VISIBLE);
+        } else {
+            // A HomeFragment nem látható, tehát elrejtjük a gombot
+            button.setVisibility(View.GONE);
+        }
+    }
 }
+
